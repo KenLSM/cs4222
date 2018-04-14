89e2c4e5a6c5f4127ec7b891bcd186810d24355e
abellimz <abellim1309@gmail.com>
2018-04-14 19:29:35 +0800

    Add the use of data buffer for classification

--- hw4/classifiers/classifier.py
+++ hw4/classifiers/classifier.py
@@ -1,7 +1,8 @@
 class Classifier:
-    """ Classifies based on sensor data and uses the current state of classifier
+    """ Classifies based on a list of sensor data for 1 time step
+        and uses the current state of classifier to return a prediction.
         Returns a state as defined in state class.
      """
-    def classify(self, sensor_data):
+    def classify(self, sensor_datas):
         raise NotImplementedError()
 
